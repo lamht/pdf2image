faae2292638cfaec6db2e123197312b7aa2ff6be
lamhuynhIFD <lam.huynh@infodation.vn>
2020-08-17 15:49:07 +0700

    init project

---
+++ pdf2image/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace pdf2image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                var output = "pdftoppm -r 300 -png input.pdf output".Bash();
+                Console.WriteLine($"Exec {output}");
+                //check output dir
+                var lsoutput = "ls -la".Bash();
+                Console.WriteLine($"Exec {lsoutput}");
+                Thread.Sleep(3000);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }        
+    }
+}
